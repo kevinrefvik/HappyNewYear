1f3ac5cb9aeb70702694a5f2b1adc9b8a6aae2c3
Kevin Refvik <kevin@bubble.dk>
2015-01-20 19:37:23 +0100

    Added readme and github link

--- app/views/newyear.blade.php
+++ app/views/newyear.blade.php
@@ -3,6 +3,7 @@
 	<head>
 		<title>Nytår</title>
 		<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,400italic,700italic,300italic,300' rel='stylesheet' type='text/css'>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 		<style>
 			body {font-family: 'Roboto Condensed', sans-serif;}
 
@@ -12,6 +13,11 @@
 			.answer {font-size: 50px; font-weight: bold; text-transform: uppercase; text-align: center;}
 			.answer.yes {color: green;}
 			.answer.no {color: red;}
+
+			.github {position: absolute; bottom: 5px; right: 5px; font-size: 13px;}
+			.github a {color: #333; text-decoration: none;}
+			.github a:hover {color: #4183C4;}
+			.github a .fa {margin-right: 5px; font-size: 15px;}
 		</style>
 	</head>
 
@@ -22,5 +28,7 @@
 		@else
 			<div class="answer no">Nej</div>
 		@endif
+
+		<div class="github"><a href="https://github.com/kevinrefvik/happynewyear" target="_blank"><i class="fa fa-github"></i>Contribute on GitHub</a></div>
 	</body>
 </html>
